6540593f87d47a8c2cb96aaa475c0c25d72a2a52
dayne leon <dayneleon94@gmail.com>
2023-03-07 21:01:03 -0300

    actualizacion de estilos e inclusion de JS

---
+++ evento_ps.js
@@ -0,0 +1,22 @@
+function mostrar_eventos(data) {
+    let body = ``;
+
+    const eventos = document.getElementById("tarjetas");
+    for (let i = 0; i < data.events.length; i++) {
+        if (data.currentDate > data.events[i].date) {
+            body += `<div class="card" style="width: 15rem;">
+    <img src="${data.events[i].image}" class="card-img-top" alt="foto">
+    <div class="card-body">
+        <h5 class="card-title">${data.events[i].name}</h5>
+        <p class="card-text">${data.events[i].description}</p>
+    </div>
+    <div class="card-body">
+        <a href="#" class="card-link">Price: ${data.events[i].price}</a>
+        <a href="./details.html" class="card-link boton_ver">Ver mas</a>
+    </div>
+</div>`;
+        }
+    }
+    eventos.innerHTML = body;
+}
+mostrar_eventos(data);
